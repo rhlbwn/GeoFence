a0283e6bd3ca18733407cb54214d5d5831392c36
Rahul <rb.270995@gmail.com>
2021-04-10 14:21:18 +0530

    Adding project to git

---
+++ GeoFence/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeoFence
+//
+//  Created by Rahul Bawane on 10/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
